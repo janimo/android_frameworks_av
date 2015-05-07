0863c272beb127b4e4b29ff85a29085296ddd2d1
Jani Monoses <jani@ubuntu.com>
2015-05-08 00:33:03 +0300

    Disable camera shutter sound. Could be better done via setting a property
    Change-Id: I30e673fcfea9c22efd2195aba0f6fd032c3d6604

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -897,6 +897,8 @@ void CameraService::loadSound() {
     property_get("persist.camera.shutter.disable", value, "0");
     int disableSound = atoi(value);
 
+    disableSound = 1;
+
     if(!disableSound) {
         mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
         mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
